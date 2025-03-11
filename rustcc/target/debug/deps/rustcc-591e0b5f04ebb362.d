78fa3d28cd0cab9ee7dedf91ee3a2fd6d5c3f1f6
TheBitty <hitsuu23@gmail.com>
2025-03-10 20:09:26 -0700

    added more files for the file struct

---
+++ rustcc/target/debug/deps/rustcc-591e0b5f04ebb362.d
@@ -0,0 +1,5 @@
+/Users/leog/RustCC/rustcc/target/debug/deps/librustcc-591e0b5f04ebb362.rmeta: src/main.rs
+
+/Users/leog/RustCC/rustcc/target/debug/deps/rustcc-591e0b5f04ebb362.d: src/main.rs
+
+src/main.rs:
