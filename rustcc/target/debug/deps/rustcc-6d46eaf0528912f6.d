552914ae05f09cdaa6af66d015752618b76fc24b
TheBitty <hitsuu23@gmail.com>
2025-03-10 18:31:20 -0700

    think I got everything working?

---
+++ rustcc/target/debug/deps/rustcc-6d46eaf0528912f6.d
@@ -0,0 +1,5 @@
+/Users/leog/RustroverProjects/RustCC/rustcc/target/debug/deps/rustcc-6d46eaf0528912f6: src/main.rs
+
+/Users/leog/RustroverProjects/RustCC/rustcc/target/debug/deps/rustcc-6d46eaf0528912f6.d: src/main.rs
+
+src/main.rs:
---
+++ rustcc/target/debug/rustcc.d
@@ -0,0 +1,1 @@
+/Users/leog/RustroverProjects/RustCC/rustcc/target/debug/rustcc: /Users/leog/RustroverProjects/RustCC/rustcc/src/main.rs
