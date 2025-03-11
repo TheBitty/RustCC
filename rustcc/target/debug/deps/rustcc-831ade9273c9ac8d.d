ce87f90500c90f613aca904b1afccfc9e51d66bb
TheBitty <hitsuu23@gmail.com>
2025-03-10 23:28:23 -0700

    deleted the logs cuz yall dont need them

--- rustcc/target/debug/deps/rustcc-831ade9273c9ac8d.d
+++
@@ -1,5 +0,0 @@
-/Users/leog/RustCC/rustcc/target/debug/deps/librustcc-831ade9273c9ac8d.rmeta: src/main.rs
-
-/Users/leog/RustCC/rustcc/target/debug/deps/rustcc-831ade9273c9ac8d.d: src/main.rs
-
-src/main.rs:
