7a11eb3a68b7fe28e449f9d534fbd13fa9eb4a67
TheBitty <hitsuu23@gmail.com>
2025-03-13 21:28:53 -0700

    Remove target directories from git tracking

--- rustcc/target/debug/deps/equivalent-317ac31ac34212da.d
+++
@@ -1,5 +0,0 @@
-/Users/leog/RustCC/rustcc/target/debug/deps/libequivalent-317ac31ac34212da.rmeta: /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.2/src/lib.rs
-
-/Users/leog/RustCC/rustcc/target/debug/deps/equivalent-317ac31ac34212da.d: /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.2/src/lib.rs
-
-/Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.2/src/lib.rs:
--- rustcc/target/debug/deps/rand_chacha-e0353fa12d49e65a.d
+++
@@ -1,7 +0,0 @@
-/Users/leog/RustCC/rustcc/target/debug/deps/librand_chacha-e0353fa12d49e65a.rmeta: /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/Users/leog/RustCC/rustcc/target/debug/deps/rand_chacha-e0353fa12d49e65a.d: /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
-/Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
-/Users/leog/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
